98c38e35c4993ecb69869c4d21854756b8ba6796
Denis Baryshev <dennybaa@gmail.com>
2016-10-31 20:07:20 +0300

    added upstart service manager support

---
+++ libraries/service_manager_upstart.rb
@@ -0,0 +1,56 @@
+module PIO
+  class ServiceManagerUpstart < ServiceBase
+    resource_name :service_manager_upstart
+
+    provides :service_manager, platform: 'ubuntu'
+
+    action :start do
+      template "/etc/init/#{service_name}.conf" do
+        path "/etc/init/#{service_name}.conf"
+        source "services/upstart/#{service_name}.conf.erb"
+        owner 'root'
+        group 'root'
+        mode '0644'
+        variables provided_service_variables
+        cookbook 'pio'
+
+        notifies restart_action, new_resource, :immediately
+        action :create
+      end
+
+      # start systemd service
+      service service_name do
+        provider Chef::Provider::Service::Upstart
+        supports new_resource.supports
+        action [:enable, :start]
+      end
+    end
+
+    action :stop do
+      service service_name do
+        provider Chef::Provider::Service::Upstart
+        supports new_resource.supports
+        action [:disable, :stop]
+        only_if { ::File.exist?("/etc/init/#{service_name}.conf") }
+      end
+    end
+
+    action :restart do
+      service service_name do
+        provider Chef::Provider::Service::Upstart
+        supports new_resource.supports
+        action :restart
+        only_if { ::File.exist?("/etc/init/#{service_name}.conf") }
+      end
+    end
+
+    action :reload do
+      service service_name do
+        provider Chef::Provider::Service::Upstart
+        supports new_resource.supports
+        action :reload
+        only_if { ::File.exist?("/etc/init/#{service_name}.conf") }
+      end
+    end
+  end
+end
---
+++ recipes/aio.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: pio
+# Recipe:: aio
+#
+# Copyright 2016 ActionML LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+include_recipe 'pio::default'
+include_recipe 'pio::hadoop'
