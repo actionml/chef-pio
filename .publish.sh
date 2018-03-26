f2b11667ce43cf7100b07ffc8dd3d6f45b3172c2
Denis Baryshev <dennybaa@gmail.com>
2018-03-26 17:07:08 +0300

    removed publish script

--- .publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-: ${PUBLISHURI:?Must be given}
-
-curl -s \
-    --header "Content-Type: application/json" \
-    --data "{\"build_parameters\": {\"release_tag\": \"$TRAVIS_TAG\"}}" \
-    --request POST $PUBLISHURI > /dev/null
